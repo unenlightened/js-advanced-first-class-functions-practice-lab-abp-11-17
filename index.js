51706ca3edf52fbb6725f2dd0833951dcd0b599d
Chely Ho <adept_inept@yahoo.com>
2017-11-07 20:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Code your solution in this file!
 
-function logDriverNames(array){
-  array.forEach(function(array){
+function logDriverNames (array) {
+  array.forEach(function (array){
     console.log(array.name);
   });
+  
   return array;
 }
+
+
